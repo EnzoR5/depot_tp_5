9b6626751add3dcbad6a9483054f7cfac25dd718
Enzo ROMERO <enzo.romero0505@gmail.com>
2023-12-02 12:46:37 +0100

    Commit du projet maven

---
+++ nomprojet/src/main/java/com/enzoromero/depot_tp_5/App.java
@@ -0,0 +1,13 @@
+package com.enzoromero.depot_tp_5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
